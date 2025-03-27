281da352051fa34a5bd8e809487b147334975b68
hansbenjamin <hansnuque4@gmail.com>
2025-03-27 13:24:32 -0600

    model update

--- scripts/statistical_analysis.R
+++ scripts/statistical_analysis.R
@@ -41,22 +41,17 @@ mod7 <- lm(log_wage ~ immigrant + female + educ + tenure + prov +
              naics_21 + noc_43 + union, 
            data = clean_dt)
 
-# add family characteristics
+# add family characteristics which makes all controls
 mod8 <- lm(log_wage ~ immigrant + female + educ + tenure + prov +
              naics_21 + noc_43 + union + marstat, 
-           data = clean_dt)
-
-# All controls
-mod9 <- lm(log_wage ~ immigrant + female + educ + tenure + naics_21 +
-             region,
            data = clean_dt)
 
 
 # Output the results into one table
 results <- htmlreg(
-  l = list(mod1, mod2, mod3, mod4, mod5, mod6, mod7, mod8, mod9),
+  l = list(mod1, mod2, mod3, mod4, mod5, mod6, mod7, mod8),
   file = "resultsv2.doc",
-  single.row = TRUE,
+  single.row = FALSE,
   custom.coef.map = list("immigrantEstablished immigrant" = "Established immigrant",
                          "immigrantRecent immigrant" = "Recent immigrant"),
   stars = c(0.001, 0.01, 0.05, 0.1),
